b5b7a746915de441a839b9ca9769dd8087c1fc97
thatknitter <kblewis.flt@gmail.com>
2014-10-28 09:41:28 -0500

    Conway's game of life, initial work.

---
+++ scripts/main.js
@@ -0,0 +1,32 @@
+function matrixCreator(x,y){
+	var matrix = [];
+	for(var i=0; i<x; i++){
+		matrix[i] = [];
+		for(var j=0; j<y; j++){
+			matrix[i][j] = Math.round(Math.random());
+			//if(matrix[i][j] === 1){
+				//display the color black in the cell
+			//}else{
+				//display the color white in the cell
+			//}
+		}
+	}
+	return matrix;
+}
+
+document.addEventListener("DOMContentLoaded", function buildTable(){
+	var table = document.querySelector('.table');
+	var matrix = matrixCreator(3, 3);
+	var row;
+	for(var i=0; i<matrix.length; i++){
+		row = matrix[i];
+		var tr = document.createElement("tr");
+		for(var j=0; j<row.length; j++){
+			item = row[j];
+			var td = document.createElement("td");
+			td.textContent = item;
+			tr.appendChild(td);
+		}
+		table.appendChild(tr);
+	}
+});
